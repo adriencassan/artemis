9777ee60bf7147d1cfe82c54400ac51631157c70
Adrien Cassan <adrien.cassan.tech@gmail.com>
2018-09-23 15:10:43 +0200

    commit

--- app/models/stock_rating.rb
+++ app/models/stock_rating.rb
@@ -2,6 +2,17 @@ class StockRating < ApplicationRecord
   belongs_to :profile
   belongs_to :stock
 
-  def create
+  def ago
+    (Date.today - self.created_at.to_date).to_i
+  end
+
+  def color_tag
+    if value == "Buy"
+      "green"
+    elsif value == "Neutral"
+      "gray"
+    else
+      "red"
+    end
   end
 end
